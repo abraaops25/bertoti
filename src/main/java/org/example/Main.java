53b9bd25efcdca3fffb114bd9e0124c6e941d6f7
Abraão Prado <abraao25.gojira15@gmail.com>
2026-05-14 20:42:56 -0300

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.github.ollama4j.Ollama;
+import io.github.ollama4j.exceptions.OllamaException;
+import io.github.ollama4j.models.chat.OllamaChatMessageRole;
+import io.github.ollama4j.models.chat.OllamaChatRequest;
+import io.github.ollama4j.models.chat.OllamaChatResult;
+
+public class Main {
+    public static <Ollama> void main(String[] args) throws OllamaException {
+        Ollama ollama;
+        ollama = new Ollama("http://localhost:11434/");
+        String model = "qwen2.5-coder:7b";
+
+        ollama.setRequestTimeoutSeconds(100);
+        OllamaChatRequest builder = OllamaChatRequest.builder().withModel(model);
+
+        //create first user question
+        OllamaChatRequest requestModel = builder.withMessage(OllamaChatMessageRole.USER, "What is the capital of England?").build();
+
+        //start conversation with model
+        OllamaChatResult chatResult = ollama.chat(requestModel, null);
+
+        System.out.println("First answer: " + chatResult.getResponseModel().getMessage().getResponse());
+    }
+}
